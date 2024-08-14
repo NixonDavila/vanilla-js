7d512aeb77174e6e1f4483e43a0f250108679cc2
Nixon Davila <davilanixon8@gmail.com>
2024-08-14 09:22:26 -0600

    cambios

--- src/ejercicio1.js
+++ src/ejercicio1.js
@@ -1,8 +1,12 @@
 let contador = {
   valor: 0,
   siguiente: function () {
-    // Inserte el código aquí
+    this.valor ++
+    return this.valor
   },
+  
 };
+console.log(contador.siguiente());
 
-export { contador }
+
+//export { contador }
--- src/ejercicio2.js
+++ src/ejercicio2.js
@@ -1,7 +1,17 @@
-class Contador {
-  // Inserte el código aquí
+class contador {
+  constructor() {
+    this.valor = 0;
+  }
+
+
+  siguiente(){
+    this.valor ++
+    return  this.valor
+  }
 }
 
 // Inserte el código aquí
+const instancia = new contador()
+console.log(instancia.siguiente());
 
-export { instancia }
+// export { instancia }
